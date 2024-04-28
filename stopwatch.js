61409f9273bb10095c6ec23bcb63ee340f1c3702
Yu Chen <chenyu.bnu@gmail.com>
2024-04-27 23:43:36 -0700

    Implementation of basic functionality (Task ST2)

--- stopwatch.js
+++ stopwatch.js
@@ -4,26 +4,45 @@ let secDisply = document.getElementById('seconds');
 let msecDisplay = document.getElementById('milli_seconds');
 
 let isRunning = false;
-let intervalID;
+let msecInterval, secInterval;
 let startTime;
+let elapsedPausedTime = 0;
 
 console.log(document);
 startStopBtn.addEventListener('click', startStop);
+resetBtn.addEventListener('click', reset);
 
 function startStop() {
     console.log(isRunning);
     if(!isRunning) {
-        startTime = new Date().getTime();
-        intervalID = setInterval(updateDisplay, 1);
+        isRunning = true;
+        startTime = new Date().getTime() - elapsedPausedTime;
+        msecInterval = setInterval(updateMsecDisplay, 1);
+        secInterval = setInterval(updateSecDisplay, 1000);
     }
-
+    else {
+        isRunning = false;
+        elapsedPausedTime = new Date().getTime() - startTime;
+        clearInterval(msecInterval);
+        clearInterval(secInterval);
+    }
 }
 
 function reset() {
-
+    clearInterval(msecInterval);
+    clearInterval(secInterval);
+    msecDisplay.innerText = '000';
+    secDisply.innerText = '00';
+    elapsedPausedTime = 0;
+    isRunning = false;
 }
 
-function updateDisplay() {
-    let msec = new Date().getTime() - startTime;
+function updateMsecDisplay() {
+    let msec = (new Date().getTime() - startTime) % 1000;
     msecDisplay.innerText = msec.toString().padStart(3, '0');
+}
+
+function updateSecDisplay() {
+    let sec = Math.round((new Date().getTime() - startTime) / 1000);
+    secDisply.innerText = sec.toString().padStart(2, '0');
 }
