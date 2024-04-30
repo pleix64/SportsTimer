7ac1a3d0991c3a4fce7a0d2cff5f140675a66757
Yu Chen <chenyu.bnu@gmail.com>
2024-04-30 00:22:21 -0700

    Implementation of Persistence

--- stopwatch.js
+++ stopwatch.js
@@ -17,19 +17,42 @@ lapBtn.addEventListener('click', lap);
 
 document.addEventListener('keydown', keyHandler);
 
+if(localStorage.length != 0) {
+    isRunning = JSON.parse(localStorage.getItem('isRunning'));
+    startTime = Number(localStorage.getItem('startTime'));
+    elapsedPausedTime = Number(localStorage.getItem('elapsedPausedTime'));
+    msecDisplay.innerText = localStorage.getItem('milliSeconds');
+    secDisplay.innerText = localStorage.getItem('seconds');
+    if(isRunning) {
+        msecInterval = setInterval(updateMsecDisplay, 1);
+        secInterval = setInterval(updateSecDisplay, 1000);
+    }
+    const lapsData = localStorage.getItem('laps');
+    if(lapsData !== null) {
+        const laps = JSON.parse(lapsData);
+        laps.forEach(appendLap);
+    }
+}
+
 function startStop() {
     if(!isRunning) {
         isRunning = true;
         startTime = new Date().getTime() - elapsedPausedTime;
         msecInterval = setInterval(updateMsecDisplay, 1);
         secInterval = setInterval(updateSecDisplay, 1000);
+
+        saveDataItem('isRunning', isRunning);
+        saveDataItem('startTime', startTime);
     }
     else {
         isRunning = false;
         elapsedPausedTime = new Date().getTime() - startTime;
         clearInterval(msecInterval);
         clearInterval(secInterval);
-    }
+
+        saveDataItem('isRunning', isRunning);
+        saveDataItem('elapsedPausedTime', elapsedPausedTime);
+   }
 }
 
 function reset() {
@@ -40,31 +63,56 @@ function reset() {
     lapTimes.innerText = '';
     elapsedPausedTime = 0;
     isRunning = false;
+    localStorage.clear();
 }
 
 function lap() {
     if(isRunning) {
-        const aLapTime = document.createElement("li");
-        const lapSecDisplay = document.createElement("span");
-        const whiteSpace = document.createTextNode(" ");
-        const lapMsecDisplay = document.createElement("span");
-        lapSecDisplay.innerText = secDisplay.innerText;
-        lapMsecDisplay.innerText = msecDisplay.innerText;
-        aLapTime.appendChild(lapSecDisplay);
-        aLapTime.appendChild(whiteSpace);
-        aLapTime.appendChild(lapMsecDisplay);
-        lapTimes.appendChild(aLapTime);
+        const aLap = {
+            seconds: secDisplay.innerText,
+            milliSeconds: msecDisplay.innerText
+        }
+        appendLap(aLap);
+
+        if(localStorage.getItem('laps') == null) {
+            localStorage.setItem('laps', '[]');
+        }
+        const laps = JSON.parse(localStorage.getItem('laps'));
+        laps.push(aLap);
+        localStorage.setItem('laps', JSON.stringify(laps));
     }
 }
 
 function updateMsecDisplay() {
     let msec = (new Date().getTime() - startTime) % 1000;
     msecDisplay.innerText = msec.toString().padStart(3, '0');
+
+    saveDataItem('milliSeconds', msecDisplay.innerText);
 }
 
 function updateSecDisplay() {
     let sec = Math.round((new Date().getTime() - startTime) / 1000);
     secDisplay.innerText = sec.toString().padStart(2, '0');
+
+    saveDataItem('seconds', secDisplay.innerText);
+}
+
+function appendLap(aLap) {
+    console.log('a lap record is displayed.');
+    const aLapTime = document.createElement("li");
+    const lapSecDisplay = document.createElement("span");
+    const whiteSpace = document.createTextNode(" ");
+    const lapMsecDisplay = document.createElement("span");
+    lapSecDisplay.innerText = aLap.seconds;
+    lapMsecDisplay.innerText = aLap.milliSeconds;
+    aLapTime.appendChild(lapSecDisplay);
+    aLapTime.appendChild(whiteSpace);
+    aLapTime.appendChild(lapMsecDisplay);
+    lapTimes.appendChild(aLapTime);
+}
+
+function saveDataItem(key, value) {
+    localStorage.setItem(key, value.toString());
 }
 
 function keyHandler(event) {
